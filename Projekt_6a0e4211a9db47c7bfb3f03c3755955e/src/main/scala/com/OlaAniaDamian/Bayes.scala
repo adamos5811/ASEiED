784f958652a8ca5b0e51e270aef696ec758c4358
Damian W <winczewski.d@gmail.com>
2017-06-25 02:15:27 +0200

    Split try

--- Projekt_6a0e4211a9db47c7bfb3f03c3755955e/src/main/scala/com/OlaAniaDamian/Bayes.scala
+++ Projekt_6a0e4211a9db47c7bfb3f03c3755955e/src/main/scala/com/OlaAniaDamian/Bayes.scala
@@ -20,11 +20,36 @@ class Bayes {
   def bayesData {
     val stream = new FileInputStream("./src/main/resources/dane.json")
     val source: String = Source.fromFile("./src/main/resources/dane.json").getLines.mkString
+    println("\nSource:")
+    println(source)
+    println("\nRemove tabulacje:")
+    val source2 = source.toString.replaceAll("\t", "")
+    println(source2)
+    println("\nRemove \" :")
+    val source3 = source2.toString.replaceAll("\"", "")
+    println(source3)
+    println("\nRemove space :")
+    val source4 = source3.replaceAll(" ", "")
+    println(source4)
+    val source5 = source4.toString.split("{").mkString("")
+    println("SPlit")
+    print(source5)
     
-    val parsing = parseFull(source).get
-   // val parsing2 = parsing.get
+    
+    
+//    val source4 = source3.toString.split(' ')
+//    source4.foreach(println)
+    
+    
+    val parsing = parseFull(source)//.get
+    println("Parsing:")
     println(parsing)
+    val parsing2 = parsing.get
+    println("Parsing2:")
+    println(parsing2)
+
     
+    println("-----Przyklad-----")
     val capitals = Map("France" -> "Paris", "Japan" -> "Tokyo")
     println(capitals)
     println("capitals.get( \"France\" ) : " +  capitals.get( "France" ))
