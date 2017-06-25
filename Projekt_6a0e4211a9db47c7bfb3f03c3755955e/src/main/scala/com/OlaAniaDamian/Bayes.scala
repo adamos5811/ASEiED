b54b72693ef780d2adc5e6beb967db55a96dcc77
Damian W <winczewski.d@gmail.com>
2017-06-25 12:13:49 +0200

    extract data

--- Projekt_6a0e4211a9db47c7bfb3f03c3755955e/src/main/scala/com/OlaAniaDamian/Bayes.scala
+++ Projekt_6a0e4211a9db47c7bfb3f03c3755955e/src/main/scala/com/OlaAniaDamian/Bayes.scala
@@ -5,6 +5,7 @@ import org.apache.spark.sql.functions.col
 import java.io.File
 import java.io.FileInputStream
 import org.json4s.jackson.Json
+import scala.collection.mutable.MutableList
 import scala.io.Source
 import scala.util.parsing.json.JSON._
 
@@ -20,49 +21,36 @@ class Bayes {
   def bayesData {
     val stream = new FileInputStream("./src/main/resources/dane.json")
     val source: String = Source.fromFile("./src/main/resources/dane.json").getLines.mkString
-    println("\nSource:")
-    val source2 = source.toString.replaceAll("\t", "")
+    val source2 = source.replaceAll("\t", "")
     val source3 = source2.replaceAll("\"", "")
     val source4 = source3.replaceAll(" ", "")
     val source5 = source4.replaceAll("\\[", "")
     val source6 = source5.split(":")(2)
     val source7 = source6.replaceAll("\\]","")
     val source8 = source7.replaceAll("\\{","")
-    val source9 = source8.replaceAll("\\{","")
-    println(source9)
+    val source9 = source8.replaceAll("\\}","")
     val data = source9.split(",")
     println("Dane:")
-    data.foreach(print)
-    val x1 = Array[Int]()
-    val y1 = Array[Int]()
-    val x2 = Array[Int]()
-    val y2 = Array[Int]()
-    for(i <- 0 to data.length){
-      x1 :+ data(i)
-      y1 :+ data(i+1)
-      x2 :+ data(i+2)
-      y2 :+ data(i+3)
-      i += 2
+    println(data.mkString(","))
+    val x1 = MutableList[Int]()
+    val y1 = MutableList[Int]()
+    val x2 = MutableList[Int]()
+    val y2 = MutableList[Int]()
+    for(i <- 0 until data.length-3 by 4){
+      x1 += data(i).toInt
+      y1 += data(i+1).toInt
+      x2 += data(i+2).toInt
+      y2 += data(i+3).toInt
     }
+    print("\nx1: ")
+    print(x1.mkString(", "))
+    print("\ny1: ")
+    print(y1.mkString(", "))
+    print("\nx2: ")
+    print(x2.mkString(", "))
+    print("\ny2: ")
+    print(y2.mkString(", "))
     
-    
-//    val source4 = source3.toString.split(' ')
-//    source4.foreach(println)
-    
-    
-    val parsing = parseFull(source)//.get
-    println("Parsing:")
-    println(parsing)
-    val parsing2 = parsing.get
-    println("Parsing2:")
-    
-    
-    println("")
-    println("-----Przyklad-----")
-    val capitals = Map("France" -> "Paris", "Japan" -> "Tokyo")
-    println(capitals)
-    println("capitals.get( \"France\" ) : " +  capitals.get( "France" ))
-    println("capitals.get( \"India\" ) : " +  capitals.get( "India" ))
-    //val parsing2 = ms get data
+
   }
 }
