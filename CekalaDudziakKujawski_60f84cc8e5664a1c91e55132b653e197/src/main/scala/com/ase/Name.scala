871beea7d378f8a0569af419ec13bd4426c43add
KujawskiCezary <kujawskicezary@wp.pl>
2017-06-26 10:14:27 +0200

    edited json + added counting

--- CekalaDudziakKujawski_60f84cc8e5664a1c91e55132b653e197/src/main/scala/com/ase/Name.scala
+++ CekalaDudziakKujawski_60f84cc8e5664a1c91e55132b653e197/src/main/scala/com/ase/Name.scala
@@ -12,8 +12,16 @@ class Name {
 
 
   def joinData: Unit = {
-	val df = sparkSession.read.json("./src/main/resources/dataMay-31-2017.json")
-	df.show()
+	
+
+	val df = sparkSession.read.json("./src/main/resources/dataMay-31-2017_corrected.json")
+	df.createOrReplaceTempView("users_view")
+	val df2 = sparkSession.sql("select name as name2, id as id2, city as city2, company as company2 from users_view")
+	val joinDs = df.join(df2, df.col("name").equalTo(df2.col("name2")) && df.col("id").notEqual(df2.col("id2")), "left")
+	joinDs.createOrReplaceTempView("names")
+	//joinDs.show()
+	sparkSession.sql("select distinct name, count(name) from names group by name").show()
+
   }
 
 }
