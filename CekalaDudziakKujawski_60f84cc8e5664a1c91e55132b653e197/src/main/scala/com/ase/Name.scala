46ea916003768d25738165ce575eda801f28f235
Krzysztof Dudziak <krz.dudziak@gmail.com>
2017-06-26 19:58:10 +0200

    Another version - different representation

--- CekalaDudziakKujawski_60f84cc8e5664a1c91e55132b653e197/src/main/scala/com/ase/Name.scala
+++ CekalaDudziakKujawski_60f84cc8e5664a1c91e55132b653e197/src/main/scala/com/ase/Name.scala
@@ -19,13 +19,19 @@ class Name {
 	val df2 = sparkSession.sql("select name as name2, id as id2, city as city2, company as company2 from users_view")
 	val joinDs = df.join(df2, df.col("name").equalTo(df2.col("name2")) && df.col("id").notEqual(df2.col("id2")), "left")
 	joinDs.createOrReplaceTempView("names")
-	val people = sparkSession.sql("select distinct name, (count(name) / round(sqrt(count(name)))) as amount from names group by name")
-	val data0 = people.collect().toSeq
-	val data1 = for (i <- data0) yield (i(0).toString, i(1).asInstanceOf[Double])
+	/**val people = sparkSession.sql("select distinct name, city,company,(count(name) / round(sqrt(count(name)))) as amount from names group by city,company,name order by amount desc")*/
+        /**val people = sparkSession.sql("select name, city, company,(floor(count(name)/ round(1+sqrt(count(name)-1)))) as amount from names group by city,company,name order by amount desc")*/
+        /**val people = sparkSession.sql("select name,count(name) as amount from names group by name order by amount desc")*/
+        val people = sparkSession.sql("select name, city, company,round((count(name))/(1+sqrt(count(name)))) as amount from names group by city,company,name order by amount desc")
+/**,city,company order by amount desc")*/
+        people.show()
+        Thread.sleep(50000)
+        val data0 = people.collect().toSeq
+	val data1 = for (i <- data0) yield (i(0).toString + ":" + i(1).toString + ":" + i(2).toString, i(3).asInstanceOf[Double])
     val chart = PieChart(data1)
     chart.saveAsPNG("./src/main/resources/chart.png")
     chart.show()
-    Thread.sleep(5000)
+    Thread.sleep(50000)
   
   }
 }
