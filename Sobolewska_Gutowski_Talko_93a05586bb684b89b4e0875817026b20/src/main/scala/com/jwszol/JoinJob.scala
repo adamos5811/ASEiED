a4780cb2c14787e6cc86ae5ab11e69d4e49b7c4e
negocki <katalko@gmail.com>
2017-06-26 22:42:43 +0200

    dodano klase Point i wczytywanie punktów do tablic

--- Sobolewska_Gutowski_Talko_93a05586bb684b89b4e0875817026b20/src/main/scala/com/jwszol/JoinJob.scala
+++ Sobolewska_Gutowski_Talko_93a05586bb684b89b4e0875817026b20/src/main/scala/com/jwszol/JoinJob.scala
@@ -1,8 +1,6 @@
 package com.jwszol
 
 
-// $example on$
-
 import org.apache.spark.mllib.classification.{NaiveBayes, NaiveBayesModel}
 
 import org.apache.spark.mllib.util.MLUtils.loadLibSVMFile
@@ -14,7 +12,15 @@ import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions.explode
 import org.apache.spark.sql.Column
 import org.apache.spark.sql.functions._
-// $example off$
+import org.apache.spark.mllib.linalg.{Vector, Vectors}
+import org.apache.spark.mllib.regression.LabeledPoint
+import scala.collection.mutable.ArrayBuffer
+
+import java.awt._
+import java.awt.geom._
+import java.awt.image.BufferedImage
+import javax.swing.{ImageIcon, JOptionPane}
+
 
 
 
@@ -26,54 +32,41 @@ object NaiveBayesExample {
 
 
     val conf = new SparkConf().setMaster("local[2]") 
-                    .setAppName("NaiveBayesExample")
+                    .setAppName("NaiveBayes")
     val sc = new SparkContext(conf)
 
     val sqlContext = new org.apache.spark.sql.SQLContext(sc) //tworzymy context do operacji sql na danych
-    // $example on$
-
-    // Load and parse the data file.
-
-    val data = loadLibSVMFile(sc, "./src/main/resources/sample_libsvm_data.txt")
 
     val path = "./src/main/resources/dataMay-31-2017.json" //wczytywanie json
     val MapPartRDD = sc.wholeTextFiles(path).values
     val rawData = sqlContext.read.json(MapPartRDD)
     val extractedData = rawData.withColumn("data", explode(rawData.col("data"))).select("data")
     extractedData.createOrReplaceTempView("pairs_view")
-
-    val punkty1x = sqlContext.sql("SELECT cast(data[0] as float) as x FROM pairs_view").cache() //ladujemy punkty
-    val punkty1y = sqlContext.sql("SELECT cast(data[1] as float) as y FROM pairs_view").cache()
-    val punkty2x = sqlContext.sql("SELECT cast(data[2] as float) as x FROM pairs_view").cache()
-    val punkty2y = sqlContext.sql("SELECT cast(data[3] as float) as y FROM pairs_view").cache()
     
-    punkty1x.collect().foreach(println) //collect daje nam dostep do punktow
+    val punkty1x = sqlContext.sql("SELECT cast(data[0] as integer) as x FROM pairs_view") //ladujemy osobno wspolrzedne
+    val punkty1y = sqlContext.sql("SELECT cast(data[1] as integer) as y FROM pairs_view")
+    val punkty2x = sqlContext.sql("SELECT cast(data[2] as integer) as x FROM pairs_view")
+    val punkty2y = sqlContext.sql("SELECT cast(data[3] as integer) as y FROM pairs_view")
 
+    var bluePoints: ArrayBuffer[Point] = new ArrayBuffer[Point]()
+    var redPoints: ArrayBuffer[Point] = new ArrayBuffer[Point]()
+    var pktx1 = punkty1x.collect()
+    var pktx2 = punkty2x.collect()
+    var pkty1 = punkty1y.collect()
+    var pkty2 = punkty2y.collect()
+    
+    for(i <- 0 to pktx1.length - 1)
+    {
+        var bluePoint = new Point(pktx1(i).getInt(0), pkty1(i).getInt(0), Color.BLUE) //dodajemy kazdy punkt do ArrayBuffer
+        var redPoint = new Point(pktx2(i).getInt(0), pkty2(i).getInt(0), Color.BLUE)
+        bluePoints += bluePoint
+        redPoints += redPoint
+    }
+
+    
     // Split data into training (60%) and test (40%).
 
     val Array(training, test) = data.randomSplit(Array(0.6, 0.4))
-
-
-
-    val model = NaiveBayes.train(training, lambda = 1.0, modelType = "multinomial")
-
-
-
-    val predictionAndLabel = test.map(p => (model.predict(p.features), p.label))
-
-    val accuracy = 1.0 * predictionAndLabel.filter(x => x._1 == x._2).count() / test.count()
-
-
-
-    // Save and load model
-
-    // model.save(sc, "./src/main/resources/myNaiveBayesModel")
-
-    // val sameModel = NaiveBayesModel.load(sc, "./src/main/resources/myNaiveBayesModel")
-
-    // $example off$
-
-
 
     sc.stop()
 
---
+++ Sobolewska_Gutowski_Talko_93a05586bb684b89b4e0875817026b20/src/main/scala/com/jwszol/Point.scala
@@ -0,0 +1,7 @@
+package com.jwszol
+
+import java.awt.Color
+
+class Point(var x: Long, var y: Long, var color: Color) extends Serializable {
+
+}
