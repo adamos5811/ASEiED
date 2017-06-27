d2857d1eb9d9d11aac3c1867d927ceabde9a91c9
Krzysztof Dudziak <krz.dudziak@gmail.com>
2017-06-27 09:21:47 +0200

    Last changes

--- CekalaDudziakKujawski_60f84cc8e5664a1c91e55132b653e197/src/main/scala/com/ase/Name.scala
+++ CekalaDudziakKujawski_60f84cc8e5664a1c91e55132b653e197/src/main/scala/com/ase/Name.scala
@@ -17,12 +17,14 @@ class Name {
 	val df = sparkSession.read.json("./src/main/resources/dataMay-31-2017_corrected.json")
 	df.createOrReplaceTempView("users_view")
 	val df2 = sparkSession.sql("select name as name2, id as id2, city as city2, company as company2 from users_view")
-	val joinDs = df.join(df2, df.col("name").equalTo(df2.col("name2")) && df.col("id").notEqual(df2.col("id2")), "left")
-	joinDs.createOrReplaceTempView("names")
+	/**val joinDs = df.join(df2, df.col("name").equalTo(df2.col("name2")) && df.col("id").notEqual(df2.col("id2")), "left")*/
+	val joinDs = df
+        joinDs.createOrReplaceTempView("names")
 	/**val people = sparkSession.sql("select distinct name, city,company,(count(name) / round(sqrt(count(name)))) as amount from names group by city,company,name order by amount desc")*/
         /**val people = sparkSession.sql("select name, city, company,(floor(count(name)/ round(1+sqrt(count(name)-1)))) as amount from names group by city,company,name order by amount desc")*/
         /**val people = sparkSession.sql("select name,count(name) as amount from names group by name order by amount desc")*/
-        val people = sparkSession.sql("select name, city, company,round((count(name))/(1+sqrt(count(name)))) as amount from names group by city,company,name order by amount desc")
+        /**val people = sparkSession.sql("select name, city, company,round((count(name))/(1+sqrt(count(name)))) as amount from names group by city,company,name order by amount desc")*/
+        val people = sparkSession.sql("select name, city, company,(count(name)) as amount from names group by city,company,name order by amount desc")
 /**,city,company order by amount desc")*/
         people.show()
         Thread.sleep(50000)
